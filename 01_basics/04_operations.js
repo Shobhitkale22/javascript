0812e1dbe1f7c6acb0a014203704c254c0f8f4d9
Shobhitkale22 <kaleshobhit9@gmail.com>
2026-06-25 17:23:45 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,24 @@
+let value = 3
+let negvalue = - value
+//console.log(negvalue);
+/*
+console.log(2+2);
+console.log(2*2);
+console.log(2-2);
+and all other mathematical operations
+*/
+  
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log( (3+4) * 5 % 3);
+
+console.log(+true);  
