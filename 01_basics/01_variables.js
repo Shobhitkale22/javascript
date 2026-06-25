8ef2ae5f49a5b3e2bca21b47affce25c0d84f657
Shobhitkale22 <kaleshobhit9@gmail.com>
2026-06-25 11:50:15 +0000

    variabkes and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1441232
+let accountemail = "dannysupare12@gmail.com"
+var accountpassword = "231423"
+accountCity = "latur"
+let accountstate;
+
+//accountId = 2//not allowed because it is constant
+/*
+prefer not to use var becuase of issue in block scope and functional scope
+*/
+accountemail = "hscsh12@gmail.com"
+accountpassword ="21222212"
+console.log(accountId);
+console.table([accountId, accountemail , accountpassword,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+//alert("hello")// we are using node not web browser
+
+console.log(3+
+    3 ) // code readability should be high
+console.log("hello")
+
+let name = "histesh"
+let age = 18
+let isLoggedIn = false
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean=> true/false
+null=> standalone value
+undefined =>
+symbol => unique
+object
+*/
+
+console.log(typeof null)
