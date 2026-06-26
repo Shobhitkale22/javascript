1848acc3e2277a6d3f420a1cabffb93e06268f14
Shobhitkale22 <kaleshobhit9@gmail.com>
2026-06-26 21:19:10 +0000

    strings

---
+++ 01_basics/08_string.js
@@ -0,0 +1,31 @@
+ const name = "hitesh"
+ const repocount = 50
+
+ //console.log(name + repocount + "value")
+
+ console.log(`hello my name is ${name} and my repocount is ${repocount} `); 
+const gamename = new String('shobhit')
+ console.log(gamename[0]);
+ console.log(gamename.__proto__)
+
+ console.log(gamename.length);
+ console.log(gamename.toUpperCase())
+ //charAt
+ //indexOf
+ //startsWith - it works not only for single letter but also for words
+
+ const newstring = gamename.substring(-2,4)
+ console.log(newstring) 
+
+ const anotherstring = gamename.slice(-8 , 4)
+ console.log(anotherstring)
+
+ //if u give slice from neg value it will ignore it and start from 0 
+ 
+ const newstringone = "   sbhoss      "
+ console.log(newstringone.trim())
+ //trimstart and trimend
+ //.replace to replace something 
+ //.includes to check whether something is there or not
+
+ 
