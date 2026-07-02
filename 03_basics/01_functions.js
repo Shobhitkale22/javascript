999af268ccd10091867013209c26b6af2dfcfc04
Shobhitkale22 <kaleshobhit9@gmail.com>
2026-07-02 20:13:01 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,12 +12,12 @@ function addtwonumbers( number1 , number2){
     //console.log(number1+number2)
     //let result = number1 + number2
     //return result
-   // return number1 + number2 
+    return number1 + number2 
 }
 
 const result = addtwonumbers(3, 4)
 
-//console.log("Result :" , result)
+console.log("Result :" , result)
 
 function loginusermsg(username = "sam"){
     
@@ -32,4 +32,32 @@ function loginusermsg(username = "sam"){
 
 
 
-function 
+function calculatecartPrice(...num1){  // ... is rest operator which compacts the whole valuess into a array
+
+    return num1
+
+}
+//console.log(calculatecartPrice(200 , 400 , 500 , 2000)
+
+
+
+const user = {
+    username: "shobhit",
+    price: 277
+}
+
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username}`)
+}
+
+//handleobject(user)
+handleobject(user)
+
+const mynewarray = [200,3939,22,1122]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+//console.log(returnSecondValue(mynewarray))
+// or you can directly write returnsecondvalue([23728,54,5,6546,645])
