859a409dcdd99d875fd99bb264bc77b4379fe3ee
Shobhitkale22 <kaleshobhit9@gmail.com>
2026-07-03 13:59:52 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+let a = 10
+const b = 20
+var c = 30 // and this is global scope this values can be accessed inside any function
+
+if(true){
+    let a = 10 
+    const b = 20
+}  // the values under the function is block scope this values cannot be accessed outside the block 
+
+function 
