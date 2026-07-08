9d79a2f40d8bfb50e9a2a33834e75964e4a0b260
Shobhitkale22 <kaleshobhit9@gmail.com>
2026-07-08 19:00:09 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+// if 
+
+//if (true){
+
+//}
+//if (false){
+    
+//}
+
+const isuserloggedin = true
+
+if (isuserloggedin){
+console.log("executed")
+}
+ 
+// < , > , <=, >= , ==, != , ===, !==
+
+if (tempreature === 40){
+    //console.log("less than 50")
+    
+}else{
+  //  console.log("tempreature is greater than 50")
+}
+
+//console.log("executed ")// this line will surely execute but above if else will check the condition and then it will execute 
+
+const score = 200
+
+if (score> 100){
+    const power = "fly" // if u use var here it will print becasue var works in global scope
+    //console.log(`user power : $(power)`)
+}
+
+ //console.log(`user power : $(power)`) // this will cause problrem beacuse of global scope problem
+
+
+ const balance = 1000
+ 
+ if (balance<500) {
+     //console.log("less than 500") }
+
+     else if (balance<750){
+        //console.log("less than 750")
+
+
+     }
+     else if(balance<900){
+       // console.log("less than 750")
+     }
+     else {
+     //   console.log("less than 1200")
+
+     }
