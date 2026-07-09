f6c2d6d005dc81107e0b70de78c728b8187e0718
Shobhitkale22 <kaleshobhit9@gmail.com>
2026-07-10 02:24:40 +0530

    two , three

---
+++ 05_iterators/three.js
@@ -0,0 +1,3 @@
+//for of
+//this loop is used to iterate over iterable objects like arrays, strings, maps, sets, etc. It allows you to loop through the values of an iterable object without having to use an index or a counter variable.
+
---
+++ 05_iterators/two.js
@@ -0,0 +1,23 @@
+let index = 0 
+
+while(index <= 10){
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myarray=["flash","batsman","superman"]
+
+let arr = 0
+
+while(arr < myarray.length){
+    console.log(`value of arr is ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 1
+do {
+
+console.log(`score is ${score}`);
+score++
+} while(score <= 10)
+
